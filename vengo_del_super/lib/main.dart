7b208b143a49ead1702909fa5bcd40dabcd4b180
IgTriguero <itrigueropedraza@gmail.com>
2020-04-04 18:26:20 +0200

    Añadida ruta

--- vengo_del_super/lib/main.dart
+++ vengo_del_super/lib/main.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
             CustomerSubmitAddressScreen.routeName: (context) =>
                 CustomerSubmitAddressScreen(),
             HacerCompraScreen.routeName: (context) => HacerCompraScreen(),
+            ConfirmarCompraScreen.routeName: (context) => ConfirmarCompraScreen(),
           },
         ),
       ),
