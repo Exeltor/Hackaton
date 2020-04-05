8bc63fd0a11459a097396eda25b5c8a2038147ae
IgTriguero <itrigueropedraza@gmail.com>
2020-04-05 15:35:03 +0200

    Cogidos y sin coger separados

--- vengo_del_super/lib/screens/pedidos_activos.dart
+++ vengo_del_super/lib/screens/pedidos_activos.dart
@@ -15,11 +15,44 @@ class PedidosActivosScreen extends StatefulWidget {
 }
 
 class _PedidosActivosScreenState extends State<PedidosActivosScreen> {
-  _buildPedidos(AsyncSnapshot<QuerySnapshot> snapshot, BuildContext context) {
+  _buildSinCoger(AsyncSnapshot<QuerySnapshot> snapshot, BuildContext context) {
     return ListView(
         children: snapshot.data.documents
             .where((DocumentSnapshot document) =>
-                document['usuario'] == Provider.of<AuthService>(context).uid)
+                document['usuario'] == Provider.of<AuthService>(context).uid &&
+                document['repartidor'] == null)
+            .toList()
+            .map((DocumentSnapshot document) => InkWell(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ConfirmarPedidoScreen(
+                          pedidoId: document.documentID,
+                          userId: document.data['usuario'],
+                          distancia: 0),
+                    ),
+                  ),
+                  child: ListTile(
+                    key: ValueKey(document.documentID),
+                    title: FutureBuilder(
+                      future: Provider.of<DatabaseService>(context)
+                          .userMap(document['usuario']),
+                      builder: (context, snapshot) =>
+                          snapshot.connectionState == ConnectionState.waiting
+                              ? Center(child: CircularProgressIndicator())
+                              : Text(snapshot.data.nombre),
+                    ),
+                    subtitle: Text(document['localizacion']['direccion']),
+                  ),
+                ))
+            .toList());
+  }
+
+  _buildCogidos(AsyncSnapshot<QuerySnapshot> snapshot, BuildContext context) {
+    return ListView(
+        children: snapshot.data.documents
+            .where((DocumentSnapshot document) =>
+                document['usuario'] == Provider.of<AuthService>(context).uid &&
+                document['repartidor'] != null)
             .toList()
             .map((DocumentSnapshot document) => InkWell(
                   onTap: () => Navigator.of(context).push(
@@ -48,45 +81,48 @@ class _PedidosActivosScreenState extends State<PedidosActivosScreen> {
 
   _buildAceptados(AsyncSnapshot<QuerySnapshot> snapshot, BuildContext context) {
     return ListView(
-    children: snapshot.data.documents
-        .where((DocumentSnapshot document) =>
-            document['repartidor'] == Provider.of<AuthService>(context).uid)
-        .toList()
-        .map((DocumentSnapshot document) => InkWell(
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ConfirmarPedidoScreen(
-                      pedidoId: document.documentID,
-                      userId: document.data['usuario'],
-                      distancia: 0),
-                ),
-              ),
-              child: ListTile(
-                key: ValueKey(document.documentID),
-                title: FutureBuilder(
-                  future: Provider.of<DatabaseService>(context)
-                      .userMap(document['usuario']),
-                  builder: (context, snapshot) =>
-                      snapshot.connectionState == ConnectionState.waiting
-                          ? Center(child: CircularProgressIndicator())
-                          : Text(snapshot.data.nombre),
-                ),
-                subtitle: Text(document['localizacion']['direccion']),
-              ),
-            ))
-        .toList());
+        children: snapshot.data.documents
+            .where((DocumentSnapshot document) =>
+                document['repartidor'] == Provider.of<AuthService>(context).uid)
+            .toList()
+            .map((DocumentSnapshot document) => InkWell(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ConfirmarPedidoScreen(
+                          pedidoId: document.documentID,
+                          userId: document.data['usuario'],
+                          distancia: 0),
+                    ),
+                  ),
+                  child: ListTile(
+                    key: ValueKey(document.documentID),
+                    title: FutureBuilder(
+                      future: Provider.of<DatabaseService>(context)
+                          .userMap(document['usuario']),
+                      builder: (context, snapshot) =>
+                          snapshot.connectionState == ConnectionState.waiting
+                              ? Center(child: CircularProgressIndicator())
+                              : Text(snapshot.data.nombre),
+                    ),
+                    subtitle: Text(document['localizacion']['direccion']),
+                  ),
+                ))
+            .toList());
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         drawer: AppDrawer(),
         appBar: AppBar(
           title: Text('Pedidos Activos'),
           bottom: TabBar(tabs: [
-            Tab(child: Text('Pedidos')),
+            Tab(child: Text('Sin coger')),
+            Tab(
+              child: Text('Cogidos'),
+            ),
             Tab(child: Text('Aceptados')),
           ]),
         ),
@@ -106,7 +142,7 @@ class _PedidosActivosScreenState extends State<PedidosActivosScreen> {
                         return Text('Cargando...');
                       default:
                         return Expanded(
-                          child: _buildPedidos(snapshot, context),
+                          child: _buildSinCoger(snapshot, context),
                         );
                     }
                   },
@@ -120,7 +156,29 @@ class _PedidosActivosScreenState extends State<PedidosActivosScreen> {
                       .listasCompra,
                   builder: (BuildContext context,
                       AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+                    if (snapshot.hasError)
+                      return Text('Error: ${snapshot.error}');
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        return Text('Cargando...');
+                      default:
+                        return Expanded(
+                          child: _buildCogidos(snapshot, context),
+                        );
+                    }
+                  },
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                StreamBuilder<QuerySnapshot>(
+                  stream: Provider.of<DatabaseService>(context, listen: false)
+                      .listasCompra,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasError)
+                      return Text('Error: ${snapshot.error}');
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
                         return Text('Cargando...');
